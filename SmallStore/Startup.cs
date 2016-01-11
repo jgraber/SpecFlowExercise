ca051a4b252737d5712a3d212e153a24de0199d4
Johnny Graber <jg@jgraber.ch>
2016-01-11 22:20:02 +0100

    add basic web template for MVC

---
+++ SmallStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmallStore.Startup))]
+namespace SmallStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
