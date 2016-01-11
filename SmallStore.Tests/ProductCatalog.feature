83181442ae8e6172f1024d3ecf29c795dfe61645
Johnny Graber <jg@jgraber.ch>
2016-01-11 22:47:45 +0100

    add first test in SpecFlow

---
+++ SmallStore.Tests/ProductCatalog.feature
@@ -0,0 +1,8 @@
+﻿Feature: ProductCatalog
+	In order to browse the product catalog
+	I want to see a list of products
+
+Scenario: Browsing the catalog
+	Given the product catalog has 5 items
+	When I navigate to the store
+	Then I find those 5 items on the start page
---
+++ SmallStore.Tests/ProductCatalogSteps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using TechTalk.SpecFlow;
+
+namespace SmallStore.Tests
+{
+    [Binding]
+    public class ProductCatalogSteps
+    {
+        [Given(@"the product catalog has (.*) items")]
+        public void GivenTheProductCatalogHasItems(int p0)
+        {
+            //ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I navigate to the store")]
+        public void WhenINavigateToTheStore()
+        {
+        }
+
+        [Then(@"I find those (.*) items on the start page")]
+        public void ThenIFindThoseItemsOnTheStartPage(int numberOfItems)
+        {
+            var storePage = "http://localhost:8717/";
+            using (var driver = new FirefoxDriver())
+            {
+                driver.Navigate().GoToUrl(storePage);
+                var itemsOnPage = driver.FindElementById("itemCount").Text;
+                Assert.AreEqual(numberOfItems.ToString(), itemsOnPage);
+                driver.Quit();
+            }
+
+
+        }
+    }
+}
