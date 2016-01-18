706dd354c04134b8bb30de80f68ae9725c063704
Johnny Graber <jg@jgraber.ch>
2016-01-18 22:30:28 +0100

    Beispiel in Deutsch mit ersten Schritten

---
+++ SmallStore.German.Tests/KorrektesRunden.feature
@@ -0,0 +1,11 @@
+﻿# language: de-DE
+# encoding: utf-8
+Funktionalität: Korrektes Runden
+	Damit jeder erhält was er verdient, muss korrekt auf 
+	den nächsten 0.5er Schritt gerundet werden.
+
+@mytag
+Szenario: Gerade Nummer wird nicht gerundet
+	Angenommen der Eintrag enthält 1 Credit
+	Wenn das Protokoll angeschaut wird
+	Dann wird dem Benutzer 1 Credit angezeigt
---
+++ SmallStore.German.Tests/RundenSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SmallStore.German.Tests
+{
+    [Binding]
+    public class RundenSteps
+    {
+        [Given(@"der Eintrag enthält (.*) Credit")]
+        public void AngenommenDerEintragEnthaltCredit(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"das Protokoll angeschaut wird")]
+        public void WennDasProtokollAngeschautWird()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"wird dem Benutzer (.*) Credit angezeigt")]
+        public void DannWirdDemBenutzerCreditAngezeigt(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
