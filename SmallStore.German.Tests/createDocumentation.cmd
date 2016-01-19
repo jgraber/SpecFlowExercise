d818186e06affcc516e94ac9bb54c00ac050950f
Johnny Graber <jg@jgraber.ch>
2016-01-19 22:40:28 +0100

    add pickles command line tool to generate report

---
+++ SmallStore.German.Tests/createDocumentation.cmd
@@ -0,0 +1,6 @@
+..\packages\Pickles.CommandLine.1.2.3\tools\pickles.exe^
+	--language=de-CH^
+    --feature-directory=.\^
+    --output-directory=.\documentation^
+    --link-results-file=.\bin\Debug\TestResult.xml^
+    --documentation-format=dhtml
---
+++ SmallStore.Tests/createDocumentation.cmd
@@ -0,0 +1,6 @@
+..\packages\Pickles.CommandLine.2.2.0\tools\pickles.exe^
+	--language=en^
+    --feature-directory=.\^
+    --output-directory=.\documentation^
+    --link-results-file=.\bin\Debug\TestResult.xml^
+    --documentation-format=dhtml
